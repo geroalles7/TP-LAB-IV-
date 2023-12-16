84b01f6cd6f6feccbb51fcbfcb4dd172fd29e85f
Lungo <augustodittler@gmail.com>
2023-12-16 17:43:22 -0300

    dejé piola el ver info de la laptop

--- tp-final/src/componentes/laptop-info.jsx
+++ tp-final/src/componentes/laptop-info.jsx
@@ -6,6 +6,9 @@ export default function LaptopsInfo() {
   const [laptop, setLaptop] = useState(null);
   const [error, setError] = useState(null);
   const { id } = useParams();
+  //prueba para la imagen aleatoria
+  const timestamp = new Date().getTime();
+  const imageUrl = `https://source.unsplash.com/500x500/?laptop&t=${timestamp}`;
 
   useEffect(() => {
     get_laptop(parseInt(id, 10))
@@ -28,38 +31,63 @@ export default function LaptopsInfo() {
   }
 
   return (
-    <>
-      <div className="h1">{laptop.modelo}</div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Marca</th>
-            <th>Modelo</th>
-            <th>Ram</th>
-            <th>Tipo del Disco</th>
-            <th>Marca del Disco</th>
-            <th>Modelo del Disco</th>
-            <th>Tamaño del Disco</th>
-            <th>Placa</th>
-            <th>Precio</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{laptop.id}</td>
-            <td>{laptop.marca}</td>
-            <td>{laptop.modelo}</td>
-            <td>{laptop.ram}</td>
-            <td>{laptop.tipoDisco}</td>
-            <td>{laptop.marcaDisco}</td>
-            <td>{laptop.modeloDisco}</td>
-            <td>{laptop.tamanoDisco}</td>
-            <td>{laptop.placa}</td>
-            <td>{laptop.precio}</td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+    <div className="container">
+      <div className="h1 text-center">
+        <i>{laptop.marca}</i>. Modelo: <u>{laptop.modelo}</u>
+      </div>
+      <div className="row">
+        <div className="col">
+          <table className="table table-striped table-hover table-bordered">
+            <tbody>
+              <tr>
+                <th>ID</th>
+                <td>{laptop.id}</td>
+              </tr>
+              <tr>
+                <th>Marca</th>
+                <td>{laptop.marca}</td>
+              </tr>
+              <tr>
+                <th>Modelo</th>
+                <td>{laptop.modelo}</td>
+              </tr>
+              <tr>
+                <th>Ram</th>
+                <td>{laptop.ram}</td>
+              </tr>
+              <tr>
+                <th>Tipo de disco</th>
+                <td>{laptop.tipoDisco}</td>
+              </tr>
+              <tr>
+                <th>Marca del disco</th>
+                <td>{laptop.marcaDisco}</td>
+              </tr>
+              <tr>
+                <th>Modelo del Disco</th>
+                <td>{laptop.modeloDisco}</td>
+              </tr>
+              <tr>
+                <th>Tamaño del Disco</th>
+                <td>{laptop.tamanoDisco}</td>
+              </tr>
+              <tr>
+                <th>Placa</th>
+                <td>{laptop.placa}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="col">
+          <img src={imageUrl} alt="Laptop" />
+        </div>
+      </div>
+    </div>
   );
 }
+/* Otra variante de otro sitio
+<img
+  src="https://picsum.photos/500/500/?random&category=technology"
+  alt="Laptop"
+/>
+*/
