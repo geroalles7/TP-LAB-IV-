211fbfe20fbeae1930131ba76f6be70f47870f4a
Lungo <augustodittler@gmail.com>
2023-12-17 19:40:14 -0300

    cambio el navbar

--- tp-final/src/App.jsx
+++ tp-final/src/App.jsx
@@ -3,6 +3,40 @@ import { Outlet, NavLink, Link } from "react-router-dom";
 function App() {
   return (
     <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <img
+            src="src/assets/icon-laptop.png"
+            alt="logo"
+            className="navbar-brand-logo me-3"
+          />
+          <NavLink to="/" className="navbar-brand h1">
+            LaptopLand
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to="laptops" className="nav-link active">
+                  Laptops
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/*}
       <nav className="navbar navbar-expand-lg navbar-dark bg-success border-bottom border-body">
         <div className="container-fluid align-items-center p-4">
           <img
@@ -40,7 +74,7 @@ function App() {
           </div>
         </div>
       </nav>
-
+  */}
       <Outlet />
 
       <footer className="fixed-bottom w-100 p-3 bg-light">
