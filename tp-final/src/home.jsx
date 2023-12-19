246520422cff27a16422437fa1d84fd7169b155b
Lungo <augustodittler@gmail.com>
2023-12-19 15:46:10 -0300

    instalo reactstrap

--- tp-final/src/home.jsx
+++ tp-final/src/home.jsx
@@ -1,19 +1,28 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Jumbotron } from "react-bootstrap";
 
 export default function Home() {
+  const jumbotronStyle = {
+    background:
+      "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://source.unsplash.com/1920x1080/?laptop')",
+    backgroundSize: "cover",
+    color: "white",
+  };
+
   return (
-    <Container className="py-5">
-      <Row className="text-center">
-        <Col>
-          <h1 className="display-3">Bienvenido a CyberWorld</h1>
-          <p className="lead">
-            Tu destino para las mejores laptops del futuro.
-          </p>
-          <Button variant="primary" size="lg" href="/laptops">
-            Explorar Laptops
-          </Button>
-        </Col>
-      </Row>
-    </Container>
+    <Jumbotron fluid style={jumbotronStyle}>
+      <Container className="py-5">
+        <Row className="text-center">
+          <Col>
+            <h1 className="display-3">Bienvenido a CyberWorld</h1>
+            <p className="lead">
+              Tu destino para las mejores laptops del futuro.
+            </p>
+            <Button variant="primary" size="lg" href="/laptops">
+              Explorar Laptops
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
   );
 }
